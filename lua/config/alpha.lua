bd168c30df9b1b570187d72636bbe82f95a77b49
n67094 <n67094@fedora.fritz.box>
2024-06-07 17:44:49 +0200

    chore: migrate packer to lazy

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -1,68 +1,67 @@
 local Module = {}
 
 function Module.setup()
-	local status, alpha = pcall(require, "alpha")
-	if not status then
-		print("The plugin alpha is not installed")
-		return
-	end
+  local status, alpha = pcall(require, "alpha")
+  if not status then
+    print("The plugin alpha is not installed")
+    return
+  end
 
-	local dashboard = require("alpha.themes.dashboard")
-	local function header()
-		return {
-			[[ ]],
-			[[ ]],
-			[[ ]],
-			[[ ]],
-			[[ ]],
-			[[ ]],
-			[[ ]],
-			[[ ]],
-			[[           $$$$$$\  $$$$$$$$\  $$$$$$\   $$$$$$\  $$\   $$\ ]],
-			[[          $$  __$$\ \____$$  |$$$ __$$\ $$  __$$\ $$ |  $$ |]],
-			[[$$$$$$$\  $$ /  \__|    $$  / $$$$\ $$ |$$ /  $$ |$$ |  $$ |]],
-			[[$$  __$$\ $$$$$$$\     $$  /  $$\$$\$$ |\$$$$$$$ |$$$$$$$$ |]],
-			[[$$ |  $$ |$$  __$$\   $$  /   $$ \$$$$ | \____$$ |\_____$$ |]],
-			[[$$ |  $$ |$$ /  $$ | $$  /    $$ |\$$$ |$$\   $$ |      $$ |]],
-			[[$$ |  $$ | $$$$$$  |$$  /     \$$$$$$  /\$$$$$$  |      $$ |]],
-			[[\__|  \__| \______/ \__/       \______/  \______/       \__|]],
-		}
-	end
+  local dashboard = require("alpha.themes.dashboard")
+  local function header()
+    return {
+      [[ ]],
+      [[ ]],
+      [[ ]],
+      [[ ]],
+      [[ ]],
+      [[ ]],
+      [[ ]],
+      [[ ]],
+      [[           $$$$$$\  $$$$$$$$\  $$$$$$\   $$$$$$\  $$\   $$\ ]],
+      [[          $$  __$$\ \____$$  |$$$ __$$\ $$  __$$\ $$ |  $$ |]],
+      [[$$$$$$$\  $$ /  \__|    $$  / $$$$\ $$ |$$ /  $$ |$$ |  $$ |]],
+      [[$$  __$$\ $$$$$$$\     $$  /  $$\$$\$$ |\$$$$$$$ |$$$$$$$$ |]],
+      [[$$ |  $$ |$$  __$$\   $$  /   $$ \$$$$ | \____$$ |\_____$$ |]],
+      [[$$ |  $$ |$$ /  $$ | $$  /    $$ |\$$$ |$$\   $$ |      $$ |]],
+      [[$$ |  $$ | $$$$$$  |$$  /     \$$$$$$  /\$$$$$$  |      $$ |]],
+      [[\__|  \__| \______/ \__/       \______/  \______/       \__|]],
+    }
+  end
 
-	dashboard.section.header.val = header()
+  dashboard.section.header.val = header()
 
-	dashboard.section.buttons.val = {
-		dashboard.button("e", "  New file", ":ene <bar> startinsert <cr>"),
-		dashboard.button("p", "󰢷  Pick a file     (;fp)", ":NnnPicker<cr>"),
-		dashboard.button("f", "  Find a file     (;ff)", ":Telescope find_files hidden=true<cr>"),
-		dashboard.button("g", "  Search in file  (;fg)", ":Telescope live_grep<cr>"),
-		dashboard.button("c", "  Configuration", ":e $MYVIMRC <cr>"),
-		dashboard.button("q", "󰿅  Quit Neovim", ":qa<cr>"),
-	}
+  dashboard.section.buttons.val = {
+    dashboard.button("e", "  New file", ":ene <bar> startinsert <cr>"),
+    dashboard.button("p", "󰢷  Pick a file     (;fp)", ":NnnPicker<cr>"),
+    dashboard.button("f", "  Find a file     (;ff)", ":Telescope find_files hidden=true<cr>"),
+    dashboard.button("g", "  Search in file  (;fg)", ":Telescope live_grep<cr>"),
+    dashboard.button("c", "  Configuration", ":e $MYVIMRC <cr>"),
+    dashboard.button("q", "󰿅  Quit Neovim", ":qa<cr>"),
+  }
 
-	local function footer()
-		-- Number of plugins
-		local total_plugins = #vim.tbl_keys(packer_plugins)
-		local datetime = os.date("%d-%m-%Y  %H:%M:%S")
-		local plugins_text = "\t" .. total_plugins .. " plugins  " .. datetime
+  local function footer()
+    -- Number of plugins
+    local datetime = os.date("%d-%m-%Y  %H:%M:%S")
+    local plugins_text = "\t" .. datetime
 
-		-- Quote
-		local fortune = require("alpha.fortune")
-		local quote = table.concat(fortune(), "\n")
+    -- Quote
+    local fortune = require("alpha.fortune")
+    local quote = table.concat(fortune(), "\n")
 
-		return plugins_text .. "\n" .. quote
-	end
+    return plugins_text .. "\n" .. quote
+  end
 
-	dashboard.section.footer.val = footer()
+  dashboard.section.footer.val = footer()
 
-	dashboard.section.footer.opts.hl = "Constant"
-	dashboard.section.header.opts.hl = "Include"
-	dashboard.section.buttons.opts.hl = "Function"
-	dashboard.section.buttons.opts.hl_shortcut = "Type"
-	dashboard.opts.opts.noautocmd = true
-	dashboard.opts.opts.redraw = true
+  dashboard.section.footer.opts.hl = "Constant"
+  dashboard.section.header.opts.hl = "Include"
+  dashboard.section.buttons.opts.hl = "Function"
+  dashboard.section.buttons.opts.hl_shortcut = "Type"
+  dashboard.opts.opts.noautocmd = true
+  dashboard.opts.opts.redraw = true
 
-	alpha.setup(dashboard.opts)
+  alpha.setup(dashboard.opts)
 end
 
 return Module
